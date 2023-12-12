5e5893e01b3226b3afd258f42dc0169a62ecb4cb
Alan F <alan_ferreyra@live.com>
2023-12-12 17:55:19 -0300

    Commit inicial

---
+++ index.js
@@ -0,0 +1,26 @@
+const form = document.getElementById("form-comparacao");
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  let CampoA = document.getElementById("campo-A").value;
+  let CampoB = document.getElementById("campo-B").value;
+  let mensagemSucesso = "<b>Sucesso</b>";
+  let MensagemInvalido = "<b>Invalido</b>";
+
+  if (CampoA < CampoB) {
+    let containerMensagemSucesso = document.querySelector("#mensagem");
+    containerMensagemSucesso.innerHTML = mensagemSucesso;
+    containerMensagemSucesso.style.display = "block";
+    containerMensagemSucesso.style.backgroundColor = "#4caf50";
+    CampoA = "";
+    CampoB = "";
+  } else {
+    let containerMensagemInvalido = document.querySelector("#mensagem");
+    containerMensagemInvalido.innerHTML = MensagemInvalido;
+    containerMensagemInvalido.style.display = "block";
+    containerMensagemInvalido.style.backgroundColor = "#ff0000";
+    CampoA = "";
+    CampoB = "";
+  }
+});
